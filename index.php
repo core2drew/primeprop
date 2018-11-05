64d12cf01f1b035b2c5cafa8ceedc01efada0aa9
core2drew <calupesandro@gmail.com>
2018-11-06 01:00:14 +0800

    change filetype

--- index.php
+++ index.php
@@ -22,10 +22,10 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">About Us</a>
+            <a class="nav-link" href="/">About Us</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Ayala Land</a>
+            <a class="nav-link" href="/">Ayala Land</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Rockwell</a>
